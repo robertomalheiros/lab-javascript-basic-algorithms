c31039a9b9cc95cac7e69fb1358ed963d1584a50
robertomalheiros <robertomalheirosb@gmail.com>
2022-10-19 20:53:57 -0300

    Bonus1

--- js/index.js
+++ js/index.js
@@ -43,5 +43,16 @@ if (comp == -1) {
 } else {
     console.log('Invalid name!')
 }
-
 //BÔNUS 1
+
+const texto = `Nulla ullamcorper ligula et nibh tempus consectetur. Donec mattis tortor sed augue cursus, in varius massa gravida. Vivamus at eros elit. Praesent facilisis a ligula tincidunt finibus. Nulla nec imperdiet dui. Mauris tristique arcu fringilla massa ultrices dapibus. Nunc gravida felis eget dui luctus, consequat faucibus dolor efficitur.`
+
+
+
+const palavras = texto.split(' ').filter(palavra => palavra.length >1);
+const et = texto.split(' ').filter(palavra => palavra == 'et');
+
+console.log(`Quantidade de palavras ${palavras.length}.
+Quantidade de ocorrências de "et" ${et.length}.`)
+
+
